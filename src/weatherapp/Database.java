0db4fc5284a127f530f096a14fc09421a4aaf56e
shall220 <zpnq@internode.on.net>
2016-05-27 16:07:13 +0930

    null

---
+++ src/weatherapp/Database.java
@@ -0,0 +1,14 @@
+//A university project
+package weatherapp;
+
+import java.util.ArrayList;
+
+
+public interface Database{
+    
+    
+    public void loadObservationsFromHTMLFile();
+    
+    public ArrayList getObservations();   
+    
+}
